33a38b99d011ed095e8c5c097f9ff256ec3cba1d
Akhil-1802 <akhilmaindola18@gmail.com>
2026-07-02 22:25:39 +0530

    cors error fixed again

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -30,7 +30,7 @@ export default function App() {
     setLoading(true);
 
     try {
-      const res = await fetch(`${import.meta.env.VITE_API_URL}chat`, {
+      const res = await fetch(`http://127.0.0.1:8000/chat`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: query }),
