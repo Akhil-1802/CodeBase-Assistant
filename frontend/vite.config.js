5eede200ca132e38c5a9b6459536a1d1d04ec4af
Akhil-1802 <akhilmaindola18@gmail.com>
2026-07-02 21:39:11 +0530

    cors error fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,10 +6,9 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      "/api": {
-        target: "http://localhost:8000",
+      "/chat": {
+        target: "http://127.0.0.1:8000/api",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
